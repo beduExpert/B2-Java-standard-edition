1416172180d84087ba1b4ba5412a255ed6b7f03b
izumyry <56565204+izumyry@users.noreply.github.com>
2019-10-29 13:16:02 -0600

    Add files via upload

---
+++ Sesion-06/Reto-01/Figuras/src/Cuadrado.java
@@ -0,0 +1,4 @@
+
+public class Cuadrado {
+	public int lado=3;
+}
---
+++ Sesion-06/Reto-01/Figuras/src/FigurasTest.java
@@ -0,0 +1,12 @@
+
+public class FigurasTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circulo c = new Circulo(); //Declarando instancia de tipo Círculo;
+		Triangulo t = new Triangulo(); //Declarando instancia de tipo Triángulo;
+		Cuadrado cu = new Cuadrado(); //Declarando instancia de tipo Cuadrado;
+		System.out.println("Teminando de crear instancias...");
+	}
+
+}
