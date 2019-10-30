f8d8944c0bbb72cb6ab2e415969600f3e4bd84ac
izumyry <56565204+izumyry@users.noreply.github.com>
2019-10-30 00:09:40 -0600

    Add files via upload

---
+++ Sesion-06/Ejemplo-04/Gimnasio/src/GimnasioTest.java
@@ -0,0 +1,20 @@
+public class GimnasioTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		
+		Socio s = Socio.registroSocio(); //Invocación del método registroSocio(static), retornará la instancia de tipo Socio creada.
+		s.addActividad( new Yoga() );
+		s.addActividad( new Pilates() );
+		s.addActividad( new Trx() );
+		s.addActividad( new Yoga() );;
+		
+		System.out.println("************************************************************");
+		System.out.println("¿Qué ejercicio hace el socio? " + s.getNumeroSocio());
+		for( Actividad temp : s.getActividades() ) {
+			temp.ejecutarActividad();
+		}
+			
+	}
+	
+}
---
+++ Sesion-06/Ejemplo-04/Gimnasio/src/Pilates.java
@@ -0,0 +1,6 @@
+
+public class Pilates extends Actividad {
+	public void ejecutarActividad() {
+		System.out.println("Pilates time...");
+	}
+}
