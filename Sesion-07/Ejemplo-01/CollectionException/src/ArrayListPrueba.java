fec966fab6887e1c903957854c6db45752d4ae50
izumyry <56565204+izumyry@users.noreply.github.com>
2019-10-31 20:43:04 -0600

    Add files via upload

--- Sesion-07/Ejemplo-01/CollectionException/src/ArrayListPrueba.java
+++ Sesion-07/Ejemplo-01/CollectionException/src/ArrayListPrueba.java
@@ -4,45 +4,40 @@ import java.util.Collections;
 public class ArrayListPrueba {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		//1.- Arreglo
 		/************************ Arreglo ********************/
 
-		String[] array1 = new String[3];
-		array1[0]="Manuel";
-		array1[1]="Izumy";
-		array1[2]="Nochipa";
+		int[] array1 = new int[3];
+		array1[0]=13;
+		array1[1]=30;
+		array1[2]=5;
 
-		array1 = new String[6];		
+		array1 = new int[6];		
 		
-		for( String temp : array1) {
+		for( int temp : array1) {
 			System.out.println(temp);
 		}
 
 		//2.- ArrayList
 		/************************ ArrayList de nombres ********************/
-	    /*ArrayList<String> groupNames = new ArrayList<String>();
-		groupNames.add("Manuel");
-		groupNames.add("Izumy");
-		groupNames.add("Nochipa");
+	    /*ArrayList<Integer> groupNames = new ArrayList<Integer>();
+		groupNames.add(13);
+		groupNames.add(30);
+		groupNames.add(5);
 		
 		System.out.println("Imprimiendo ArrayList: " + groupNames);
 	    System.out.println("Tamaño ArrayList: " + groupNames.size());		
 		System.out.println("Obteniendo objeto en la posición 1 del ArrayList: " + groupNames.get(1));		
 		System.out.println("Eliminando Objeto en la posición 2 del ArrayList: " + groupNames.remove(2)); 
-		System.out.println("Imprimiendo ArrayList: " + groupNames);*/
-		
-		//3.- Collections
-		/************************ Collections.sort ********************/
-		
-		/*Collections.sort(groupNames);
-		System.out.println(groupNames);*/
+		System.out.println("Imprimiendo ArrayList: " + groupNames);*/	
 		
 		//4.- Valor repetido
 		/*********************** Valor repetido en ArrayList ***************/
 		
-		/*groupNames.add("Izumy");
-		System.out.println(groupNames);*/
+		/*groupNames.add(13);
+		System.out.println(groupNames);*/			
 			
 	}
 
