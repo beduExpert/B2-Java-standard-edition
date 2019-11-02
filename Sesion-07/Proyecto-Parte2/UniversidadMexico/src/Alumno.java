08f3b6cb20fad86095a6b08b9fcf296272b971c9
izumyry <56565204+izumyry@users.noreply.github.com>
2019-11-01 23:18:03 -0600

    Add files via upload

---
+++ Sesion-07/Proyecto-Parte2/UniversidadMexico/src/Alumno.java
@@ -0,0 +1,84 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Alumno {
+	 private String nombre;//Nombre
+	 private int numeroCuenta;//NúmeroCuenta
+	 private String correoElectronico;//CorreoElectrónico
+	 private final int MAX_LIBROS = 3;
+     private ArrayList<Libro> listaLibros = new ArrayList<Libro>();
+     private static Path p = Paths.get("archivo.txt"); //Generando objeto de tipo Path para manipular archivo.txt
+
+	 
+	 public Alumno(String nombre, int numeroCuenta, String correElectronico) {		
+		 this.nombre=nombre;
+		 this.numeroCuenta=numeroCuenta;
+		 this.correoElectronico=correElectronico;
+	 }
+	 
+	 public String toString() {
+		 return getNombre() + " " + getNumeroCuenta();
+	 }
+	 
+	 public void addLibro(Libro nuevo) {
+		 if( listaLibros.size() == MAX_LIBROS) {
+			 System.out.println("No puedes solicitar más libros...");
+		 }else {
+			 listaLibros.add(nuevo);
+			 System.out.println("Libro agregado...");
+		 }		 
+	 }
+	 
+	 public static ArrayList<Alumno> registraAlumno() {		
+		 	System.out.println("************** BIENVENIDO A REGISTRO ALUMNO ***************");
+		 	ArrayList<Alumno> lista =new ArrayList<Alumno>();		 
+		 	try {
+		 		Scanner input = new Scanner( new File(p.toString()) );
+		 		while(input.hasNext()) {
+		 			String nombre = input.next();
+					int numeroCuenta = input.nextInt();
+					String correoElectronico = input.next();
+					
+					/****CREANDO INSTANCIA****/
+					Alumno a = new Alumno(nombre, numeroCuenta, correoElectronico);
+					lista.add(a);
+					System.out.println("Éxito al registrar alumno: " + a.nombre);
+					System.out.println("************************************************************");						
+		 		}		 		
+		 	}catch(FileNotFoundException e) {
+		 		System.out.println("Archivo no encontrado");
+		 	}
+			return lista;			
+	 }
+	 
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getNumeroCuenta() {
+		return numeroCuenta;
+	}
+	public void setNumeroCuenta(int numeroCuenta) {
+		this.numeroCuenta = numeroCuenta;
+	}
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+	public ArrayList<Libro> getListaLibros() {
+		return listaLibros;
+	}
+	public void setListaLibros(ArrayList<Libro> listaLibros) {
+		this.listaLibros = listaLibros;
+	}
+	 
+	 
+}
---
+++ Sesion-07/Proyecto-Parte2/UniversidadMexico/src/FilosofiaLibro.java
@@ -0,0 +1,6 @@
+
+public class FilosofiaLibro extends Libro{
+	public void tiempoRetorno(){
+		System.out.println("Libro Filosofía, se retorna en 10 días...");
+	}
+}
