9af2a54e585074c209cdc699d96b65a9bbd936f8
izumyry <56565204+izumyry@users.noreply.github.com>
2019-12-04 21:13:06 -0600

    Delete Socio.java

--- Sesion-03/Ejemplo-03/Gimnasio/src/Socio.java
+++
@@ -1,9 +0,0 @@
-
-public class Socio {
-	public String nombre;            //nombre
-	public int numeroSocio;          //numeroSocio
-	public String correoElectronico; //correoElectronico
-	public String telefono;          //telefono
-	//actividades
-}
-	
