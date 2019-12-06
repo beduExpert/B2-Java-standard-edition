bcf55fe9a4d808578e3f591dfdbce00ef35a6848
izumyry <56565204+izumyry@users.noreply.github.com>
2019-12-06 14:45:22 -0600

    Add files via upload

--- Sesion-05/Ejemplo-02/Gimnasio/src/GimnasioTest.java
+++ Sesion-05/Ejemplo-02/Gimnasio/src/GimnasioTest.java
@@ -4,37 +4,7 @@ public class GimnasioTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
 		/************************* Registro Socio ****************************/
-		Scanner input = new Scanner(System.in);
-		System.out.println("************* Bienvenido al registro de socios: *************");
-		System.out.print("Ingresa el nombre del socio nuevo: ");
-		String nombre = input.next();
-		System.out.print("Ingresa el número de socio: ");
-		int numeroSocio = input.nextInt();
-		System.out.print("Ingresa el correo electrónico de socio: ");
-		String correoElectronico = input.next();
-		System.out.print("Ingresa el teléfono del socio: ");
-		String telefono=input.next();	
-		System.out.print("Ingresa tu edad: ");
-		int edad = input.nextInt();
-		if(edad < 18) {
-			System.out.println("************************************************************");
-			System.out.println("Problema - Solicitud rechazada...");
-			System.out.println("Edad mínima para registrarse: 18");
-			input.close();
-			return;
-		}
-		input.close();
-		
-		Socio s1 = new Socio();
-		s1.setNombre(nombre);
-		s1.setNumeroSocio(numeroSocio);
-		s1.setCorreoElectronico(correoElectronico);
-		s1.setTelefono(telefono);
-		
-		System.out.println("Exito al registrar socio número: " + s1.getNumeroSocio() );
-		System.out.println("*************************************************************");
-		/*********************************************************************************/
-		
+		Socio s = Socio.registroSocio();		
 	}
 	
 }
--- Sesion-05/Ejemplo-02/Gimnasio/src/Socio.java
+++ Sesion-05/Ejemplo-02/Gimnasio/src/Socio.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Socio {
 	private String nombre;            //nombre
@@ -6,6 +7,43 @@ public class Socio {
 	private String telefono;          //telefono
 	private int edad;				  //edad
 	
+	 public Socio( String nombre, int numeroSocio, String correoElectronico, String telefono, int edad) {
+	 		this.nombre=nombre;
+	 		this.numeroSocio=numeroSocio;
+	 		this.correoElectronico=correoElectronico;
+	 		this.telefono=telefono;
+	 		this.edad=edad;
+	 	}
+	 
+	public static Socio registroSocio(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("************* Bienvenido al registro de socios: *************");
+		System.out.print("Ingresa el nombre del socio nuevo: ");
+		String nombre = input.next();
+		System.out.print("Ingresa el número de socio: ");
+		int numeroSocio = input.nextInt();
+		System.out.print("Ingresa el correo electrónico de socio: ");
+		String correoElectronico = input.next();
+		System.out.print("Ingresa el teléfono del socio: ");
+		String telefono=input.next();	
+		System.out.print("Ingresa tu edad: ");
+		int edad = input.nextInt();
+		if(edad < 18) {
+			System.out.println("************************************************************");
+			System.out.println("Problema - Solicitud rechazada...");
+			System.out.println("Edad mínima para registrarse: 18");
+			input.close();
+			return null;
+		}
+		input.close();
+		
+		 Socio s1 = new Socio(nombre, numeroSocio, correoElectronico, telefono, edad);				
+		
+		System.out.println("Exito al registrar socio número: " + s1.getNumeroSocio() );
+		return s1;
+		/*********************************************************************************/
+	}
+	 
 	public String getNombre(){
 		return nombre;
 	}
@@ -46,5 +84,3 @@ public class Socio {
 		this.edad=edad;
 	}
 }
-
-//actividades arreglo
