0f72f5f5020f5b5db3b40cfb729cb4a032b0a3d2
izumyry <56565204+izumyry@users.noreply.github.com>
2019-12-06 13:28:33 -0600

    Delete SupermercadoTest.java

--- Sesion-02/Reto-03/Supermercado/src/SupermercadoTest.java
+++
@@ -1,9 +0,0 @@
-
-public class SupermercadoTest {
-	public static void main(String args[]) {
-		Pedido p = new Pedido();//Declarando instancia de tipo Pedido.
-		Producto pr = new Producto(); //Declarando instancia de tipo Producto
-		Usuario s = new Usuario(); //Declarando instancia de tipo Supermercado.
-		System.out.println("Terminando de declarar instancias...");
-	}
-}
