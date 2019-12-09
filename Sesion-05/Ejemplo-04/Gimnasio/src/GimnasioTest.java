62869cf3132b659d882b0dd9314d1948847f0c76
izumyry <56565204+izumyry@users.noreply.github.com>
2019-12-08 22:12:49 -0600

    Delete GimnasioTest.java

--- Sesion-05/Ejemplo-04/Gimnasio/src/GimnasioTest.java
+++
@@ -1,9 +0,0 @@
-public class GimnasioTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub		
-		
-		Socio s = Socio.registroSocio(); //Invocación del método registroSocio(static), retornará la instancia de tipo Socio creada.
-	}
-	
-}
