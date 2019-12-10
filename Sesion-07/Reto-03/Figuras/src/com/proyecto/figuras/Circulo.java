1e58f8c55b0d2f8432bc0ba8a48348c92cd70a94
izumyry <56565204+izumyry@users.noreply.github.com>
2019-12-09 19:31:02 -0600

    Add files via upload

---
+++ Sesion-07/Reto-03/Figuras/src/com/proyecto/figuras/Circulo.java
@@ -0,0 +1,13 @@
+package com.proyecto.figuras;
+
+public class Circulo extends Figura {
+	public float radio=4.5f;
+	
+	public Circulo() {
+		System.out.println("Constructor Círculo...");
+	}
+	
+	public void calculaArea() {
+		System.out.println("Calculando área Circulo: " + Math.PI * Math.pow(radio, 2) );
+	}
+}
---
+++ Sesion-07/Reto-03/Figuras/src/com/proyecto/principal/FigurasTest.java
@@ -0,0 +1,13 @@
+package com.proyecto.principal;
+import com.proyecto.figuras.*;
+
+public class FigurasTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		Circulo c = new Circulo(); //Declarando instancia de tipo Círculo;
+		Triangulo t = new Triangulo(); //Declarando instancia de tipo Triángulo;
+		Cuadrado cu = new Cuadrado(); //Declarando instancia de tipo Cuadrado;	
+	}
+
+}
